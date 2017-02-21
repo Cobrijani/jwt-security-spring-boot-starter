03f16a60b41ec1d91a83accf282b1fea5e278ed4
Stefan <cobrijani@gmail.com>
2017-02-21 18:41:41 +0100

    removed test annotations

--- src/test/java/com/github/cobrijani/JwtSecurityAutoconfigureApplicationTests.java
+++ src/test/java/com/github/cobrijani/JwtSecurityAutoconfigureApplicationTests.java
@@ -5,12 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class JwtSecurityAutoconfigureApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
 
 }
