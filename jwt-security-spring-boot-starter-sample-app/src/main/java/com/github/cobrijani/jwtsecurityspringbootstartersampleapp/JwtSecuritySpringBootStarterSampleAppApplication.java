81789dc9a07d3ef3b8abb848dfc9b71b22593f77
Stefan Bratic <stefanbraticns@gmail.com>
2019-03-20 23:15:05 +0100

    added init sampe app

---
+++ jwt-security-spring-boot-starter-sample-app/src/main/java/com/github/cobrijani/jwtsecurityspringbootstartersampleapp/JwtSecuritySpringBootStarterSampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.cobrijani.jwtsecurityspringbootstartersampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecuritySpringBootStarterSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecuritySpringBootStarterSampleAppApplication.class, args);
+	}
+
+}
